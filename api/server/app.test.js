bced0152a6f165da6cdb6d3fda8b4b1adc5aae99
wballard7 <wballard7@gmail.com>
2024-07-30 16:21:55 -0400

    WB, RS, BS; created an API folder and server subfolder. Installed nodemon and supertest dependencies; created App and began developing endpoints; create app.test and began forming TDD code.

---
+++ api/server/app.test.js
@@ -0,0 +1,20 @@
+const request = require('supertest')
+const app = require('./app.js')
+
+// beforeAll(async () => {
+//   await knex.migrate.latest();
+
+//   await knex.seed.run();
+// });
+
+// afterAll(async () => {
+//   await knex.destroy();
+// })
+
+describe('GET /results')
+
+describe('GET /users/1')
+
+describe('GET /admin/users')
+
+describe()
