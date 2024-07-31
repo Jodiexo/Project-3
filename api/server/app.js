2031f9db2fd7888d4fbcdab5b090365ef0bac4d8
wballard7 <wballard7@gmail.com>
2024-07-31 17:02:51 -0400

    refactored app.js functions to use knex

--- api/server/app.js
+++ api/server/app.js
@@ -1,15 +1,10 @@
 const { v4: uuidv4 } = require ('uuid');
 const express = require('express');
-// const knex = require('knex');
 const cors = require('cors');
 const bodyParser = require('body-parser');
-// const knexConfig = require('./knexfile');
-// const port = process.env.port || 5000;
+const knex = require('knex')(require('../knexfile.js')[process.env.NODE_ENV||'development'])
 const bcrypt = require('bcrypt');
 const app = express();
-
-// Server start
-// app.listen(port, console.log(`server running on port ${port}`));
 
 app.use(cors());
 app.use(bodyParser.json());
@@ -20,64 +15,94 @@ app.get('/', (req, res) => {
 });
 // Get all users
 app.get('/users', async (req, res) => {
-  try {
-    const users = await db('users').select('*');
-    res.json(users);
-  } catch (error) {
-    res.status(503).json({ error: 'Database error' });
-  }
+  knex('users')
+    .select('*')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
 });
 // Get a specific user by ID
 app.get('/users/:id', async (req, res) => {
-  try {
-    const user = await db('users').where({ id:req.params.id }).first();
-    res.status(200).json(user);
-  } catch (error) {
-    res.status(503).json({error: 'Database error'});
-  }
+  knex('users')
+    .select('*')
+    .where({ id:req.params.id })
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
 })
 // Get all chats
 app.get('/chats', async (req, res) => {
-  try {
-    const chats = await db('chats').select('*');
-    res.json(chats);
-  } catch (error) {
-    res.status(503).json({ error: 'Database error' });
-  }
+  knex('chats')
+    .select('*')
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
 })
 // Get a specific chat by Id
 app.get('/chat/:id', async (req, res) => {
-  try {
-    const chat = await db('chats').where({ id:req.params.id }).first();
-    res.json(chat);
-  } catch (error) {
-    res.status(503).json({error: 'Database error'});
-  }
+  knex('chats')
+    .select('*')
+    .where({ id:req.params.id })
+    .then(data => res.status(200).json(data))
+    .catch(err =>
+      res.status(404).json({
+        message:
+          'The data you are looking for could not be found. Please try again'
+      })
+    );
 })
 // Post a new message
 app.post('/messages', async (req, res) => {
-  try {
-    const { content, user_id, chat_id } = req.body;
-    const [id] = await db('messages').insert({ content, user_id, chat_id }).returning('id');
-    const message = await db('messages').where({ id }).first();
-    res.status(201).json(message);
-  } catch (error) {
-    res.status(503).json({ error: 'Database error' }); 
-  }
+  const { content, user_id, chat_id } = req.body;
+  knex('messages')
+    .insert({ content: content, user_id: user_id, chat_id: chat_id })
+    .then(data => res.status(201).json(data))
+    .catch(err => 
+      res.status(503).json({
+        message: 'Post request failed. Please try again.'
+      })
+    )
+  //   try {
+  //   const [id] = await chapp_db('messages').insert({ content, user_id, chat_id }).returning('id');
+  //   const message = await chapp_db('messages').where({ id }).first();
+  //   res.status(201).json(message);
+  // } catch (error) {
+  //   res.status(503).json({ error: 'Database error' }); 
+  // }
 })
 // Post a new user upon sign up
 app.post('/users', async (req, res) => {
-  try {
-    const { first_name, last_name, user_name, password, role, email } = req.body;
-    // incorporate bcrypt password
-    const hashedPassword = await bcrypt.hash(password, 10)
-    const id = uuidv4();
-    await db('users').insert({ id, first_name, last_name, user_name, hashedPassword, role, email });
-    const newUser = await db('users').where({ id }).first();
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(503).json({ error: 'Database error' }); 
-  }
+  const { first_name, last_name, user_name, password, role, email } = req.body;
+  const hashedPassword = await bcrypt.hash(password, 10)
+  const id = uuidv4();
+
+  knex('messages')
+    .insert({ id: id, first_name: first_name, last_name: last_name, user_name: user_name, password: hashedPassword, email: email, role: role })
+    .then(data => res.status(201).json(data))
+    .catch(err => 
+      res.status(503).json({
+        message: 'Post request failed. Please try again.'
+      })
+    )
+  // try {
+  //   await chapp_db('users').insert({ id, first_name, last_name, user_name, hashedPassword, role, email });
+  //   const newUser = await chapp_db('users').where({ id }).first();
+  //   res.status(201).json(newUser);
+  // } catch (error) {
+  //   res.status(503).json({ error: 'Database error' }); 
+  // }
 })
 
 const authRoutes = require('./routes/auth'); //checks if user password matches via routes
