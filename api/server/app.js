1c018296115fd0930bf68dfe9dcd109636cef150
wballard7 <wballard7@gmail.com>
2024-07-31 11:50:07 -0400

    Defined the app.post for server/users

--- api/server/app.js
+++ api/server/app.js
@@ -5,14 +5,6 @@ const bodyParser = require('body-parser');
 // const knexConfig = require('./knexfile');
 const port = process.env.port || 5000;
 
-<<<<<<< HEAD
-const user = {
-  "id": 69420,
-  "username": 'John Doe'
-}
-
-=======
->>>>>>> main
 const app = express();
 // Server start
 app.listen(port, console.log(`server running on port ${port}`));
@@ -32,11 +24,9 @@ app.get('/users', async (req, res) => {
   } catch (error) {
     res.status(503).json({ error: 'Database error' });
   }
-  res.send('Everyone in the Army wants to be in the Space Force');
 });
 // Get a specific user by ID
 app.get('/users/:id', async (req, res) => {
-<<<<<<< HEAD
   try {
     const user = await db('users').where({ id:req.params.id }).first();
     res.status(200).json(user);
@@ -52,7 +42,6 @@ app.get('/chats', async (req, res) => {
   } catch (error) {
     res.status(503).json({ error: 'Database error' });
   }
-  res.send('Everyone in the Army wants to be in the Space Force')
 })
 // Get a specific chat by Id
 app.get('/chat/:id', async (req, res) => {
@@ -62,7 +51,6 @@ app.get('/chat/:id', async (req, res) => {
   } catch (error) {
     res.status(503).json({error: 'Database error'});
   }
-  res.send('Everyone in the Army wants to be in the Space Force')
 })
 // Post a new message
 app.post('/messages', async (req, res) => {
@@ -74,32 +62,18 @@ app.post('/messages', async (req, res) => {
   } catch (error) {
     res.status(503).json({ error: 'Database error' }); 
   }
-  res.send('Post to /messages')
 })
-// Post a new sign up
-app.post('/signup', async (req, res) => {
-  res.send('Post to /account')
+// Post a new user upon sign up
+app.post('/server/users', async (req, res) => {
+  try {
+    const { first_name, last_name, user_name, password, role, email } = req.body;
+    const [id] = await db('users').insert({ first_name, last_name, user_name, password, role, email }).returning('id');
+    const newUser = await db('users').where({ id }).first();
+    res.status(201).json(newUser);
+  } catch (error) {
+    res.status(503).json({ error: 'Database error' }); 
+  }
 })
-=======
-  res.send('Everyone in the Army wants to be in the Space Force');
-});
-// Get all chats
-app.get('/chats', (req, res) => {
-  res.send('Everyone in the Army wants to be in the Space Force');
-});
-// Get a specific chat by Id
-app.get('/chat/:id', (req, res) => {
-  res.send('Everyone in the Army wants to be in the Space Force');
-});
-// Post a new message
-app.post('/messages', (req, res) => {
-  res.send('Post to /messages');
-});
-// Post a new sign up
-app.post('/signup', (req, res) => {
-  res.send('Post to /account');
-});
->>>>>>> main
 
 const authRoutes = require('./routes/auth'); //checks if user password matches via routes
 app.use('/fix route', authRoutes);
