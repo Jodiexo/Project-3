dc682b9408a99d7ba6e618ff64c0fd4e12cd2d5e
cuppatea <thomas.avende@gmail.com>
2024-07-31 15:25:28 -0400

    adding seeds and migrations

--- api/db/seeds/20240731184316_join_chats_table.js
+++ api/db/seeds/20240731184316_join_chats_table.js
@@ -1,13 +1,28 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
+  await knex('table_name').del();
   await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+    {
+      user_id: chats[0].chat_id,
+      chat_id: users[0].user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      user_id: chats[0].chat_id,
+      chat_id: users[1].user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      user_id: chats[1].chat_id,
+      chat_id: users[2].user_id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
   ]);
 };
