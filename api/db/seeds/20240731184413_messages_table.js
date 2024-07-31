dcd360f377721b6bb85538c7156b4f26d2d394fd
cuppatea <thomas.avende@gmail.com>
2024-07-31 15:49:41 -0400

    tables should be done

--- api/db/seeds/20240731184413_messages_table.js
+++ api/db/seeds/20240731184413_messages_table.js
@@ -1,13 +1,42 @@
 /**
  * @param { import("knex").Knex } knex
- * @returns { Promise<void> } 
+ * @returns { Promise<void> }
  */
-exports.seed = async function(knex) {
+exports.seed = async function (knex) {
   // Deletes ALL existing entries
-  await knex('table_name').del()
-  await knex('table_name').insert([
-    {id: 1, colName: 'rowValue1'},
-    {id: 2, colName: 'rowValue2'},
-    {id: 3, colName: 'rowValue3'}
+  await knex('messages').del();
+  await knex('messages').insert([
+    {
+      message_id: uuidv4(),
+      message_body: "Docker isn't real",
+      receiving_uid: users[0].id,
+      sending_uid: users[1].id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      message_id: uuidv4(),
+      message_body: "Where's dad?",
+      receiving_uid: users[1].id,
+      sending_uid: users[2].id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      message_id: uuidv4(),
+      message_body: 'Hello World!',
+      receiving_uid: users[2].id,
+      sending_uid: users[3].id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
+    {
+      message_id: uuidv4(),
+      message_body: "There ain't no m**f**n snakes on this m**f**n plane",
+      receiving_uid: users[3].id,
+      sending_uid: users[4].id,
+      created_at: new Date(),
+      updated_at: new Date(),
+    },
   ]);
 };
