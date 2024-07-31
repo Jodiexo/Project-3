280dd86dfff2f5bd167a0bcb2647f1caacf93d26
cuppatea <thomas.avende@gmail.com>
2024-07-31 14:46:43 -0400

    cleared t-homas_practice_folder from the hub

--- thomas practice folder/PRACTICE_knex.js
+++
@@ -1,8 +0,0 @@
-lkajsdlf
-
-const HOST = process.env.DATABASE_HOST || '127.0.0.1';
-const USER = process.env.POSTGRES_USER || 'postgres';
-const PASSWORD = process.env.POSTGRES_PASSWORD || 'docker';
-const DATABASE = process.env.POSTGRES_DB || 'chapp_db';
-const PORT = process.env.
-
